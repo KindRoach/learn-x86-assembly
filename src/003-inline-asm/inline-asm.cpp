3e74fdcabafff803efa484874834e8c5d89352c3
KindRoach <kindroach@hotmail.com>
2025-09-17 22:20:55 +0800

    Better example for inline-asm.cpp

--- src/003-inline-asm/inline-asm.cpp
+++ src/003-inline-asm/inline-asm.cpp
@@ -3,10 +3,11 @@
 int add_three(int a, int b, int c) {
     int result;
     asm volatile (
-        "addl %[b], %[a];" // a = a + b
-        "addl %[c], %[a];" // a = a + c
-        : [a] "=&r" (result) // output: result
-        : "[a]" (a), [b] "r" (b), [c] "r" (c) // input: a, b, c
+        "movl %[a], %[res];" // res = a
+        "addl %[b], %[res];" // res += b
+        "addl %[c], %[res];" // res += c
+        : [res] "=r" (result) // output: result = res
+        : [a] "r" (a), [b] "r" (b), [c] "r" (c) // inputs
     );
     return result;
 }
