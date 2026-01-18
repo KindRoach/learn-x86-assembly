e608c6f5d6d8ef152433bc8998625a16bd60f3c9
KindRoach <kindroach@hotmail.com>
2026-01-18 06:34:08 +0000

    Add placeholder reg example to inline-asm.

--- src/003-inline-asm/inline-asm.cpp
+++ src/003-inline-asm/inline-asm.cpp
@@ -1,19 +1,35 @@
 #include <iostream>
 
-int add_three(int a, int b, int c) {
+int add_three_placeholder_reg(int a, int b, int c)
+{
     int result;
-    asm volatile (
-        "addl %[b], %[a];" // a = a + b
-        "addl %[c], %[a];" // a = a + c
-        : [a] "=&r" (result) // output: result
-        : "[a]" (a), [b] "r" (b), [c] "r" (c) // input: a, b, c
+    asm volatile(
+        "movl %1, %0;"           // result = a
+        "addl %2, %0;"           // result += b
+        "addl %3, %0;"           // result += c
+        : "=r"(result)           // output: result in any register
+        : "r"(a), "r"(b), "r"(c) // input: a, b, c in registers
     );
     return result;
 }
 
-int main() {
+int add_three_symbolic_named_reg(int a, int b, int c)
+{
+    int result;
+    asm volatile(
+        "movl %[a], %[res];"                 // res = a
+        "addl %[b], %[res];"                 // res += b
+        "addl %[c], %[res];"                 // res += c
+        : [res] "=r"(result)                 // output: result = res
+        : [a] "r"(a), [b] "r"(b), [c] "r"(c) // inputs
+    );
+    return result;
+}
+
+int main()
+{
     int x = 5, y = 10, z = 20;
-    int res = add_three(x, y, z);
-    std::cout << x << " + " << y << " + " << z << " = " << res << std::endl;
+    std::cout << "add_three_placeholder_reg(" << x << ", " << y << ", " << z << ") = " << add_three_placeholder_reg(x, y, z) << std::endl;
+    std::cout << "add_three_symbolic_named_reg(" << x << ", " << y << ", " << z << ") = " << add_three_symbolic_named_reg(x, y, z) << std::endl;
     return 0;
 }
